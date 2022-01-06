5f53d9e8155110ae55f8e3d7f077ddd566d1313c
Vani Sachdev <sachdev.vani@gmail.com>
2022-01-05 20:31:42 -0800

    basically done w all data collecting

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ SPOTIPY_CLIENT_ID = '9f2aa702ed634cf2947e19f0cbdf17c7'
 SPOTIPY_CLIENT_SECRET = '0e98d396bd3a4ebdb3d96a23905cea99'
 SPOTIPY_REDIRECT_URI = 'http://127.0.0.1:5000'
 SCOPE = "user-top-read"
+DF_COLUMNS  = ["title", "artists", "valence", "spotify url", "album_cover url"]
+
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, 
                                                client_secret=SPOTIPY_CLIENT_SECRET, 
@@ -20,21 +22,39 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                                scope=SCOPE))
 
 
-def get_track_items(track_dictionary):
-    item_list = []
+def get_track_ids(track_dictionary):
+    id_list = []
     for song in track_dictionary:
-        item_list.append(song['id'])
+        id_list.append(song['id'])
 
-    return item_list
+    return id_list
+
+def metadata(track_id):
+    feature_dict = sp.audio_features(track_id)[0]
+    valence = feature_dict['valence']
+    song_md = sp.track(track_id)
+    title = song_md['name']
+    artists = song_md['artists'][0]['name']
+    url = song_md['external_urls']['spotify']
+    album_cover =  song_md['album']['images'][0]['url']
+
+    return [title, artists, valence, url, album_cover]
+
 
 
 #top_tracks = dictionary
-top_tracks = sp.current_user_top_artists(limit = 10, offset  = 0, time_range="short_term")
+top_tracks = sp.current_user_top_tracks(limit = 10, offset  = 0, time_range="short_term")
 track_items = top_tracks['items']
 
+all_ids = get_track_ids(track_items)
 
-all_items = get_track_items(track_items)
-all_items_first = all_items[0]
+song_data = []
+# song_data.append(DF_COLUMNS)
 
-print (all_items)
+for song in all_ids:
+    song_data.append(metadata(song))
 
+#df 
+
+song_df = pd.DataFrame(song_data, columns = DF_COLUMNS)
+print (song_df[["title", "artists", "valence"]])
