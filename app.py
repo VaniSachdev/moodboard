ca2642ad0fecb87fe144bbb9f5e8639f2c2ccc47
Vani Sachdev <sachdev.vani@gmail.com>
2022-01-08 19:12:01 -0800

    i hate css w a passion but things are coming together

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, url_for, session, redirect 
+from flask import Flask, request, url_for, session, redirect, render_template
+from matplotlib.pyplot import plot
 from analysis import SPOTIPY_CLIENT_SECRET, SPOTIPY_CLIENT_ID
 from spotipy.oauth2 import SpotifyOAuth
+from plot import plot_graph 
 
 app = Flask(__name__)
 # app.secret_key = "bruhidkforrn"
@@ -8,16 +10,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def login():
-    pass
-
-def button():
+    picture_link = "/static/media/moodboard.png"
     spot_oauth = create_spotify_oauth()
     auth_url = spot_oauth.get_authorize_url()
-    return redirect(auth_url)
+    return render_template("login.html", picture_link = picture_link, auth_url = auth_url)
+
 
 @app.route('/redirect')
 def redirect_page():
-    return 'redirect'
+    plot_graph() 
+    return render_template("explain.html")
+
+@app.route('/moodboard')
+def moodboard():
+    return render_template("moodboard.html")
 
 def create_spotify_oauth():
     return SpotifyOAuth(
--- plot.py
+++ plot.py
@@ -4,23 +4,24 @@ import numpy as np
 
 from analysis import song_df
 
+def plot_graph():
+    print ("hi")
+    song_df['complete']  = song_df['title'] + "<br>" + song_df['artists']
 
-song_df['complete']  = song_df['title'] + "<br>" + song_df['artists']
-
-title_col = ((np.asarray(song_df['complete'])).reshape(7,7))
-valence_col = ((np.asarray(song_df['valence'])).reshape(7,7))
+    title_col = ((np.asarray(song_df['complete'])).reshape(7,7))
+    valence_col = ((np.asarray(song_df['valence'])).reshape(7,7))
 
 
-fig = go.Figure(data=go.Heatmap(
-                    z= valence_col,
-                    text=title_col,
-                    colorscale = "YlGn_r", 
-                    showlegend = False,
-                    hovertemplate = '%{text}<extra></extra>'
-                    ))
+    fig = go.Figure(data=go.Heatmap(
+                        z= valence_col,
+                        text=title_col,
+                        colorscale = "YlGn_r", 
+                        showlegend = False,
+                        hovertemplate = '%{text}<extra></extra>'
+                        ))
 
-fig.update_xaxes(showticklabels=False)
-fig.update_yaxes(showticklabels=False)
-fig.update_traces(showscale=False)
+    fig.update_xaxes(showticklabels=False)
+    fig.update_yaxes(showticklabels=False)
+    fig.update_traces(showscale=False)
 
-fig.write_html('moodboard.html', auto_open=True)
+    fig.write_html('templates/moodboard.html', auto_open=False)
