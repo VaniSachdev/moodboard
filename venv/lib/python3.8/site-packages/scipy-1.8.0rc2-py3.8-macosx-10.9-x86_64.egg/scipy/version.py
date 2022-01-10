e7ebfd7d1d1d6557e8b0b163b1c67b07d32980ac
Vani Sachdev <sachdev.vani@gmail.com>
2022-01-09 19:48:44 -0800

    venv times

---
+++ venv/lib/python3.8/site-packages/scipy-1.8.0rc2-py3.8-macosx-10.9-x86_64.egg/scipy/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.8.0rc2'
+version = '1.8.0rc2'
+full_version = '1.8.0rc2'
+git_revision = '9153089'
+commit_count = '2242'
+release = True
+
+if not release:
+    version = full_version
